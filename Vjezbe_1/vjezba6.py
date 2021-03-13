5f9e785ebf1fc0a08b8df4a7b5d22f6a6726ec8f
name <name@changeme.com>
2021-03-13 19:48:49 +0100

    Popravljen kod

--- Vjezbe_1/vjezba6.py
+++ Vjezbe_1/vjezba6.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import math
 def crtez(p, q, r, x, y):
     c = int(input("Unesite 0 za crtez ili 1 za spremiti:"))
     if c == 0:
@@ -20,15 +20,16 @@ def kruznica():
     x=int(input("Unesite x: "))
     y=int(input("Unesite y: "))
     crtez(p, q, r, x, y)
-    d = r^2 - ((x-p)^2 + (y-q)^2)
-    
-    if d >= 0:
-        return("Tocka se nalazi na koordinatama ({},{}), te je unutar kruznice za {}.".format(x, y, d))
+    d =  (x-p)**2 + (y-q)**2
+    R = r**2
+    u = math.sqrt((x-p)**2 + (y-q)**2) - r
+    if d == R:
+        print("Tocka se nalazi na koordinatama ({},{}), te je na kruznici.".format(x, y))
+
+    elif d < R:
+        print("Tocka se nalazi unutar kruznice na koordinatama ({},{}), te je udaljena za {}.".format(x, y, -u))
         
-    elif d==0:
-        return("Tocka se nalazi na koordinatama ({},{}), te je na kruznici.".format(x, y))
-        
-    else:
-        return("Tocka se nalazi na koordinatama ({},{}), te je van kruznice za {}.".format(x, y, d))
+    elif d > R:
+        print("Tocka se nalazi van kruznice na koordinatama ({},{}), te je udaljena za {}.".format(x, y, u))
         
 kruznica()
