ab7f326ac67b933053892801c0b7c3a85aaee688
name <name@changeme.com>
2021-03-15 01:57:40 +0100

    finished work

--- Vjezbe_2/kosihitac.py
+++ Vjezbe_2/kosihitac.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import math
-from euclid import *
 
 def putanja_gibanja(kut, v0, t):
     kx = math.cos(kut)
@@ -11,7 +10,6 @@ def putanja_gibanja(kut, v0, t):
     for t in range(0, 11):
         x = (v0 * kx) * t
         y = v0 * ky - (0.5 * 9.8) * (t**2)
-        
         x_smjer.append(x)
         y_smjer.append(y)
     p = [i for i, j in enumerate(y_smjer) if j < 0]                        
@@ -21,9 +19,7 @@ def putanja_gibanja(kut, v0, t):
     plt.plot(x_smjer, y_smjer, 'r')
     plt.pause(3) 
     plt.close  
-    return x_smjer, y_smjer;
      
-
 def maksimalna_visina(v0, kut):
     ky = math.sin(kut)
     h = (v0**2 * ky**2) / (2 * 9.8)
@@ -46,31 +42,25 @@ def meta(kut, v0, t):
     ky = math.sin(kut)
     x_smjer = []
     y_smjer = []
-
+    lst = []
+    
     for t in range(0, 11):
-        lst = []
         x = (v0 * kx) * t
         y = v0 * ky - (0.5 * 9.8) * (t**2)
-        
         x_smjer.append(x)
         y_smjer.append(y)
-
-        R = r**2
-        d =  ((x-p)**2 + (y-q)**2) - R
-        lst.append(d)
-
-        #circ = Circle(Point2(p, q), r)
-        #line = Line2(x_smjer, y_smjer)
-        #line.distance(circ)
-        
-        #for element in lst:
-    def above (lst, minimum):
-        if min(lst) == minimum:
-            print ("Meta je pogodena.", minimum)   
+        for i in range(t):
+            R = r**2
+            d =  ((x_smjer[i]-p)**2 + (y_smjer[i]-q)**2) - R
+            lst.append(d) 
+    
+    def lst1(lst, minimum):
+        if min(lst) <= minimum:
+            print("Meta je pogodena.")    
         else:
-            print("Meta nije pogodena.", minimum)
-    above(lst, 0)          
-        
+            print("Meta nije pogodena. Najmanja udaljenost iznosi {}.".format(min(lst)))          
+    
+    lst1(lst, 0)    
     circle=plt.Circle((p, q),r, fill = False )
     plt.figure(figsize=(12,12))
     plt.gca().add_patch(circle)
