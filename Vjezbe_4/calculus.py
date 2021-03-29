b1510a46387414c11a350de6472378331fc681c2
name <name@changeme.com>
2021-03-29 13:55:04 +0200

    finished work

--- Vjezbe_4/calculus.py
+++ Vjezbe_4/calculus.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def derivacija(func, x, h):
-    return ((func(x+h)- func(x-h))/(2*h))
+    return ((func(x+h) - func(x-h))/(2*h))
 
 def der_analiticki(func, a, b, h):
     list1 = []
@@ -14,5 +14,22 @@ def der_analiticki(func, a, b, h):
         list2.append(x)
     return (list1, list2)
 
-def integracija(func, x, h):
-    return (func(x))
+def analitic(func, a, b, h):
+    list5 = []
+    list6 = []
+    for x in np.arange(a, b, h):
+        y = (func(x+h) - func(x))/(h)
+        list5.append(y)
+        list6.append(x)
+    return (list5, list6)
+
+def gornja_granica(func):
+    g_meda = 0
+    list1 = []
+    list2 = []
+    n = 100
+    h = 1/n
+    for x in range(n):
+        g_meda += func(x)*h
+        list1.append(g_meda)
+        list2.append(x)
--- Vjezbe_4/zadatak1.py
+++ Vjezbe_4/zadatak1.py
@@ -6,17 +6,23 @@ def f1(x):
     return 5*x**3 - 2*x**2 + 2*x -3
 
 def f2(x):
-    return 15*x**2 - 4*x +2
+    return 2*x**2 - 3
 
 print(calculus.derivacija(f1, 1., 0.1))
 calculus.der_analiticki(f1, -2, 2, 0.01)
 
 lista1, lista2 = calculus.der_analiticki(f1, -2, 2, 0.01)
 lista3, lista4 = calculus.der_analiticki(f1, -2, 2, 0.1)
-lista5, lista6 = calculus.der_analiticki(f2, -2, 2, 0.01)
+lista5, lista6 = calculus.analitic(f1, -2, 2, 0.01)
 s = [1]
-plt.scatter(lista2, lista1, s, 'r')
+plt.scatter(lista2, lista1, s, 'y')
 plt.scatter(lista4, lista3, s, 'b')
-plt.scatter(lista6, lista5, s, 'g')
-plt.pause(5)
+plt.plot(lista6, lista5, 'r')
+plt.pause(3)
+plt.show
+plt.clf()
+lista7, lista8 = calculus.gornja_granica(f2)
+s = [1]
+plt.scatter(lista7, lista8, s, 'b')
+plt.pause(10)
 plt.show
