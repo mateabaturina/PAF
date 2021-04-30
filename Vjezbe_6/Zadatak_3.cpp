9de0219f4e5506913a13989d846dbddeb2790c56
name <name@changeme.com>
2021-04-30 23:03:54 +0200

    finished work

--- Vjezbe_6/Zadatak_3.cpp
+++ Vjezbe_6/Zadatak_3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <iterator>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,30 +11,31 @@ void Polje_cijelih_brojeva(list <int> lista){
     for(it = lista.begin(); it != lista.end(); ++it)
         cout << '\t' << *it;
     cout << '\n';
-    
 }
 
 int main() {
 
     list<int> lista;
-    int a = 2;
-    int b = 5;
-    lista.push_back(a);
+    lista.push_back(2);
     lista.push_back(-3);
     lista.push_back(4);
     lista.push_back(0);
     lista.push_back(-6);
-    lista.push_back(b);
+    lista.push_back(5);
 
     cout << "\nOriginalna lista je : ";
     Polje_cijelih_brojeva(lista);
 
-    cout << "\nLista nakon okretanja redoslijeda clanova polja je : ";
-    lista.reverse();
+    cout << "\nLista nakon zamjene clanova polja je : ";
+    //swap(a, b);
+    using std::swap;
+    for (auto it = std::begin(lista); it != std::end(lista); it = std::adjacent_find(it, std::end(lista), std::less<int>{})) {
+    swap(*it, *std::next(it));
+    }
     Polje_cijelih_brojeva(lista);
 
-    cout << "\nLista nakon zamjene clanova polja je : ";
-    std::swap(a, b);
+    cout << "\nLista nakon okretanja redoslijeda clanova polja je : ";
+    lista.reverse();
     Polje_cijelih_brojeva(lista);
 
     cout << "\nLista nakon sortiranja clanova polja je : ";
