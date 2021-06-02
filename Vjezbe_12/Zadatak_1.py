0eed028ea40fba2b67aa12573aa48dbbda391bf8
mateabaturina <80029174+mateabaturina@users.noreply.github.com>
2021-06-02 08:48:28 +0200

    finished work

---
+++ Vjezbe_12/Zadatak_1.py
@@ -0,0 +1,24 @@
+import Gravity as gr
+import matplotlib.pyplot as plt
+
+Earth = {"xz": 1.486*10**11, "yz": 0, "mass":5.9742*10**24, "vxz":0, "vyz": 29783}
+Sun = {"xz": 0, "yz": 0, "mass":1.989*10**30, "vxz":0, "vyz": 0}
+Mercury = {"xz":1.486*10**11 * 0.466 , "yz": 0, "mass":3.3e24, "vxz":0, "vyz":47632}
+Venus = {"xz":1.486*10**11 * 0.723 , "yz": 0, "mass":4.8685e24, "vxz":0, "vyz":35020}
+Mars = {"xz":1.486*10**11 * 1.666 , "yz": 0, "mass":6.417e23, "vxz":0, "vyz":24007}
+
+
+bodies = [
+        gr.Planet( xz = Sun["xz"], yz = Sun["yz"], Mz = Sun["mass"], vxz = Sun["vxz"], vyz = Sun["vyz"], name = "Sun"),
+        gr.Planet( xz = Earth["xz"], yz = Earth["yz"], Mz = Earth["mass"], vxz = Earth["vxz"], vyz = Earth["vyz"], name = "Earth"),
+        gr.Planet( xz = Mercury["xz"], yz = Mercury["yz"], Mz = Mercury["mass"], vxz = Mercury["vxz"], vyz = Mercury["vyz"], name = "Mercury"),
+        gr.Planet( xz = Venus["xz"], yz = Venus["yz"], Mz = Venus["mass"], vxz = Venus["vxz"], vyz = Venus["vyz"], name = "Venus"),
+        gr.Planet( xz = Mars["xz"], yz = Mars["yz"], Mz = Mars["mass"], vxz = Mars["vxz"], vyz = Mars["vyz"], name = "Mars"),
+        ]
+
+dt = (60*60*24)/10
+dan = 60*60*24
+c = dan*365.242
+
+p1 = gr.Gravity(6.67408*10**(-11), dt, c*10)
+p1.Eulerova_metoda(bodies)
